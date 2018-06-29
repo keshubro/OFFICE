7ef7bcbcd10e3becc25ab2c9ce9b3593bff5633c
Arpit Agarwal - PWA <ArpitA@edelcap.com>
2018-06-29 18:12:14 +0530

    Table Added

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarFeatures from './JustTry/Test';
+import TryTry from './TryAgain';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
